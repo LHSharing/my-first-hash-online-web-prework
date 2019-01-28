8b5bbdb952726319dce7c6537a8302ec56fddad7
Lu Daley <daleyluis@gmail.com>
2019-01-28 17:37:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,25 +23,17 @@ def retrieval
 shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+                  "oil paintings" => 3          
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2 
-shipping_manifest["gun powder"] = 4
-#["gun powder"]=4
-shipping_manifest
-
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
+shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
